1cc2efeab7d4ad5c0231ee89e529263c73586fb7
fairyhunter13 <fairyhunter13@gmail.com>
2022-09-21 18:55:36 +0700

    - Fix bug @TEMPLATEINFO for SMS payload :bug: (#3) - Fix of ignoring error if the error code is successful error code :bug: - Refactor testing :recycle: - Add non integration build tag :white_check_mark: - Fix encoding of characters below 16 :bug: - Fix SEQ in ERROR can be string too in WhatsApp API :bug: - Change all int in responses to become string for flexibilities :bug:

--- errors.go
+++ errors.go
@@ -6,6 +6,7 @@ import (
 	"github.com/pkg/errors"
 	"io"
 	"net/http"
+	"strconv"
 )
 
 // List of errors in the valuefirst package.
@@ -45,7 +46,7 @@ func (e UnknownError) Error() string {
 type ErrorList []error
 
 // Error implements the error interface.
-func (e ErrorList) Error() (res string) {
+func (e *ErrorList) Error() (res string) {
 	byteSlice, _ := json.Marshal(e)
 	res = string(byteSlice)
 	return
@@ -53,5 +54,85 @@ func (e ErrorList) Error() (res string) {
 
 // Append appends error to the ErrorList.
 func (e *ErrorList) Append(err error) {
+	if err == nil {
+		return
+	}
+
 	*e = append(*e, err)
+}
+
+// ErrorCode represents the error code used in the ValueFirst platform.
+type ErrorCode string
+
+// Integer returns the integer value of the error code.
+func (e ErrorCode) Integer() int {
+	res, _ := strconv.Atoi(string(e))
+	return res
+}
+
+// List of error codes in the ValueFirst.
+const (
+	// General
+
+	ErrCodeGeneralSuccess       ErrorCode = "0"
+	ErrCodeGeneralNotConformDTD ErrorCode = "65535"
+
+	// Message Post
+
+	ErrCodeMessageInvalidSenderID ErrorCode = "28680"
+	ErrCodeMessageInvalidMessage  ErrorCode = "28681"
+
+	// Status Request
+
+	ErrCodeStatusRequestSuccess ErrorCode = "8448"
+
+	// Scheduler Related
+
+	ErrCodeSchedulerSuccess ErrorCode = "13568"
+)
+
+var ignoredErrorCode = map[ErrorCode]bool{
+	ErrCodeGeneralSuccess:       true,
+	ErrCodeStatusRequestSuccess: true,
+	ErrCodeSchedulerSuccess:     true,
+}
+
+func filterError(in *ResponseMessageAckGUIDError) (err error) {
+	if in == nil {
+		return
+	}
+
+	_, ok := ignoredErrorCode[in.Code]
+	if ok {
+		return
+	}
+
+	err = in
+	return
+}
+
+func filterErrors(ins *ResponseMessageAckGUIDErrors) (err error) {
+	if ins == nil {
+		return
+	}
+
+	var (
+		newList ResponseMessageAckGUIDErrors
+		tempErr error
+	)
+	for _, in := range *ins {
+		tempErr = filterError(&in)
+		if tempErr == nil {
+			continue
+		}
+
+		newList = append(newList, in)
+	}
+
+	if len(newList) <= 0 {
+		return
+	}
+
+	err = &newList
+	return
 }
